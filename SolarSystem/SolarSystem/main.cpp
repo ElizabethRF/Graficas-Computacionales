bcde24b19d4e109dd07b63ca46f694145bdf52c7
Elizabeth Rodriguez <elizabeth.rodriguez@konfio.mx>
2019-09-06 20:57:52 -0500

    finished project

--- SolarSystem/SolarSystem/main.cpp
+++ SolarSystem/SolarSystem/main.cpp
@@ -1,8 +1,8 @@
 /*
  * Elizabeth Rodríguez Fallas A0133435
- *
+ * Diego Gonzalez Plata A01333129
  * TC3022. Computer Graphics Course.
- *
+ * Solar System
  */
 
 // Please don't change lines 9-31 (It helps me to grade)
@@ -30,6 +30,24 @@
 #include <stdio.h>
 #include <math.h>
 
+/*
+ Program a solar system:
+ 
+ - Planets, moons and rings rotate on their own axes.
+ + Draw the planets in wireframe to see their rotation.
+ + The sun at the center, yellow.
+ + Mercury first (dark red), around the sun.
+ + Venus after mercury (dark green).
+ + Earth around the sun (blue).
+ + The moon around earth.
+ + A space station around the moon (clear gray).
+ + Mars around the sun (red).
+ + Jupiter around the sun (clear brown).
+ + Saturn around the sun (dark yellow).
+    - Draw the rings.
+ */
+
+
 int rotate = 0;
 
 
@@ -48,6 +66,7 @@ void planet(float r, float g, float b, float rotVar, float radius, float transla
         
         glRotatef(rotVar,0,1,0);
         glTranslatef(translate,0,0);
+        glRotatef(rotVar,0,1,0);
         glutWireSphere(radius,20,20);
     }
     glPopMatrix();
@@ -79,37 +98,38 @@ void display()                            // Called for each frame (about 60 tim
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);                // Clear color and depth buffers.
     glLoadIdentity();                                                // Reset 3D view matrix.
-    gluLookAt(0.0, 0.0, 10.0,                                        // Where the camera is.
+    gluLookAt(0.0, 25.0, 35.0,                                        // Where the camera is.
               0.0, 0.0, 0.0,                                        // To where the camera points at.
               0.0, 1.0, 0.0);                                        // "UP" vector.
     
     //axes(5);
-    // sun
-    planet(1,1,0,rotate,0.5,0); // RGB
+    // sun - The sun at the center, yellow.
+    planet(1,1,0,rotate,2,0); // RGB
     
-    // Mercury
-    planet(1, 0, 0,rotate,0.2,1);
+    // Mercury - Mercury first (dark red), around the sun.
+    planet(0.69, 0.21, 0.12,rotate* 1.7,0.4,4); // R 176 G 56 B 31
     
     
-    // Venus
-    planet(0, 1, 0,rotate,0.2,2);
+    // Venus - Venus after mercury (dark green).
+    planet(0.27, 0.49, 0.05,rotate * 1.6 ,0.6,7);  // R 70 G 126 B 13
     
-    // Earth
+    // Earth - Earth around the sun (blue).
     glPushMatrix();
     {
         glColor3f(0, 0, 1); // blue
+        glRotatef(rotate * 1.5 , 0, 1, 0);
+        glTranslatef(11,0,0);
         glRotatef(rotate , 0, 1, 0);
-        glTranslatef(3,0,0);
-        glutWireSphere(0.3, 36, 36);
+        glutWireSphere(0.8, 36, 36);
         
         // Moon
-        glColor3f(1, 1, 0); // grey
+        glColor3f(0.55, 0.55, 0.55); // grey R 142 G 142 B 142
         glRotatef(rotate , 0, 1, 0);
-        glTranslatef(1,0,0);
-        glutWireSphere(0.1, 36, 36);
+        glTranslatef(2,0,0);
+        glutWireSphere(0.2, 36, 36);
         
-        // Satelite
-        glColor3f(1, 1, 0); // clear gray
+        // Space station - A space station around the moon (clear gray).
+        glColor3f(0.83, 0.87, 0.89); // clear gray R 214 G 223 B 228
         glRotatef(rotate , 0, 1, 0);
         glTranslatef(1,0,0);
         glutWireSphere(0.1, 36, 36);
@@ -118,25 +138,26 @@ void display()                            // Called for each frame (about 60 tim
     }
     glPopMatrix();
     
+    // Mars - Mars around the sun (red).
+    planet(1, 0, 0,rotate * 1.4 ,0.7,15);
     
+    //Jupiter - Jupiter around the sun (clear brown).
+    planet(0.81, 0.55, 0.11,rotate * 1.2 ,1,18); // R  209 G 141 B 29
     
-    
-    
-    // Mars
-    glColor3f(1, 1, 0); // red
-    //glutWireSphere(0.5, 36, 36);
-    
-    //Jupiter
-    glColor3f(1, 0, 0); // clear brown
-    //glutWireSphere(0.5, 36, 36);
-    
-    // Saturn
-    glColor3f(1, 1, 0); // dark yellow
-    //glutWireSphere(0.5, 36, 36);
+    // Saturn - Saturn around the sun (dark yellow).
+    //planet(0.76, 0.71, 0.02,rotate ,1,21); // R 194 G 183 B 7
+   
+    glColor3f(0.76, 0.71, 0.02); // clear gray R 214 G 223 B 228
+    glRotatef(rotate , 0, 1, 0);
+    glTranslatef(21,0,0);
+    glutWireSphere(1, 36, 36);
     
     // Saturn rings
-    glColor3f(1, 1, 0); // brown
-   // glutWireSphere(0.5, 36, 36);
+    glColor3f(0.92, 0.48, 0.09); // R 235 G 123  B 23
+    glRotatef(90,1,0,0);
+    glRotatef(rotate*2.5,0,0,1);
+    glutWireTorus(0.5,2.5,10,50);
+    // glutWireSphere(0.5, 36, 36);
     
     glutSwapBuffers();                                                // Swap the hidden and visible buffers.
 }
@@ -174,3 +195,4 @@ int main(int argc, char* argv[])
     glutMainLoop();                                                    // Begin graphics program.
     return 0;                                                        // ANSI C requires a return value.
 }
+
